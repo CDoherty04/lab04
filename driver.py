1abcd53a38d1d432c93b4a729e81f937990ac58e
Charlie <charlieedoherty@gmail.com>
2024-02-21 15:57:02 -0600

    Create a menu to consolidate everything

--- driver.py
+++ driver.py
@@ -3,7 +3,7 @@ Author: Charlie Doherty
 KUID: 3115329
 Date: 2/16/24
 Lab: 04
-Last modified: 02/16/24
+Last modified: 02/21/24
 Purpose: Use recursion to solve given problems
 
 Run driver to execute the program
@@ -16,9 +16,35 @@ import fibonacci
 def main():
     """Runs the functions"""
 
-    # recursivepower.run()
-    # outbreak.run()
-    fibonacci.run()
+    while True:
+        try:
+
+            # Have the user choose a program they want to use
+            choice = int(input("With recursion...\n"
+                               "1) Get a power of a base\n"
+                               "2) Play Outbreak\n"
+                               "3) Experiment with the fibonacci sequence\n"))
+            print()
+
+            # Run choice program
+            match choice:
+                case 1:
+                    recursivepower.run()
+                case 2:
+                    outbreak.run()
+                case 3:
+                    fibonacci.run()
+
+                # Check input's range
+                case _:
+                    print("Make sure the number you give is an integer between 1-3\n")
+                    continue
+
+            # Break if a valid choice is given
+            break
+
+        except ValueError:
+            print("\nMake sure the number you give is an integer between 1-3\n")
 
 
 if __name__ == "__main__":
